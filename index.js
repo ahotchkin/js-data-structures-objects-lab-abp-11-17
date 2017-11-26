09c08774193ebe14ac73dbc6749ed9516784653e
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-11-26 21:44:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const driver = {
   name: 'Sam'
 };
 
-function updateDriverWithKeyAndValue(obj, key, value){
+function updateDriverWithKeyAndValue(driver, key, value){
   return Object.assign({}, obj, {[key]:value})
 }
 
 const newDriver = updateDriverWithKeyAndValue(
   driver, 'address', '11 Broadway'
 );
-
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
