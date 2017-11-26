1ee66ff791cdd967b79218422de6c98524a6051e
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-11-26 21:52:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 }
 
 function deleteFromDriverByKey(driver, key){
-  delete driver.name 
+  delete driver.name; 
 }
 
 let newdriver = deleteFromDriverByKey(driver, 'name')
